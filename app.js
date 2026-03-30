9bb42740171018f2e0e8f817421e2114d725bb0c
Pratham Singh <107633022+prathamsingh0116@users.noreply.github.com>
2026-03-30 08:40:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const upload = multer({ multer });
 
 //database connect 
 // const url = "mongodb://127.0.0.1:27017/wanderlust"
-const dbURL = process.env.ATLASDB_URL
+const dbURL = "mongodb+srv://rajputpratham27:Pratham2003@cluster0.wxbvzyf.mongodb.net"
 main().then(() => {
   console.log("Connected to DB");
 })
